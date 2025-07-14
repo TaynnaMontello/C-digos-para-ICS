fd0dc6a640b94dccbbef8ceaa62e4081ca5860a3
Taynná Montel <taynna.montel@escolar.ifrn.edu.br>
2025-07-14 08:07:04 -0300

    Projeto-Policia.py
    Projeto concluído pronto para transferência

---
+++ Projeto-Policia.py
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="Logo">
+            <img src="Logo_PMRN-1024x486.png" alt="Policia Militar">
+        </div>
+        <ul>
+            <a href="denúncias.html">Denúncias</a>
+            <a href="concursos.html">Concursos</a>
+            <a href="história.html">História</a>
+        </ul>
+    </header>
+
+    <div class="Início">
+        <h1>POLÍCIA MILITAR</h1>
+    </div>
+    <div class="centro">
+        <div class="imagens">
+            <a href="#"><img src="whatsapp-image-2024-12-06-at-07.37.57-1-.avif" alt="" width="400px"></a>
+            <h1>Nesse sábado (15), policiais militares do 9º Batalhão da Polícia Militar (BPM) realizaram a prisão de um homem suspeito de tráfico de drogas durante um patrulhamento no bairro Cidade da Esperança, na zona Oeste de Natal.</h1>
+        </div>
+            <div class="imagens">
+                <a href="#"><img src="2012202117323704b4e05070143ba4950f3c38fd1cc4c3.jpg" alt="" width="400px"></a>
+                <h1>
+                    O concurso PM RN para ingresso na Polícia Militar do Rio Grande do Norte está previsto no Projeto de Lei Orçamentária Anual (PLOA 2025). O documento prevê a realização de um novo concurso, com o objetivo ampliar o efetivo, formando e qualificando novos policiais militares.</h1>
+                <a href="prova.html">Saiba Mais</a>
+                </div>
+
+        <div class="imagens">
+            <a href="#"><img src="materia-.png" alt="" width="400px" height="300px"></a>
+            <h1>O mês de março é marcado pela história da luta das mulheres por mais direitos, segurança e equidade. A busca por melhorias passa de geração em geração, mas o fim da violência de gênero ainda parece estar distante. </h1>
+            <a href="femicidio.html">Saiba Mais</a>
+        </div>
+</body>
+</html>
